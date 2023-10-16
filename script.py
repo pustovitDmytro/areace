a64c1c9bbbe2b5730d4630d8faaf888cf4e1501e
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2023-10-16 12:47:53 +0300

    Chore: refactor Complex entry point

--- script.py
+++ script.py
@@ -21,7 +21,7 @@ def load_data():
 def main():
     dialog = xbmcgui.Dialog()
     items = load_data()
-    selected=dialog.select("Streams", list(map(lambda x: x['text'], items)))
+    selected=dialog.select("Streams", list([x['text'] for x in items]))
     if selected>=0:
       item = items[selected]
       cmd = 'StartAndroidActivity("","org.acestream.action.start_content","","acestream:?content_id=%s")' % item['acestream_id']
